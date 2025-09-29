40b5344c2d9bdf4ac34d8c0e12ff6187a22da116
conorbot <cwoodhayes@gmail.com>
2025-09-28 23:15:14 -0500

    added pyinit function

--- custom/conorbot.zsh
+++ custom/conorbot.zsh
@@ -1,21 +1,29 @@
 # easy aliases for stuff i do often
 alias backup="sudo rdiff-backup --api-version 201 --new backup /home/conor /media/conor/writable/conorbot-backups"
 alias reload="source ~/.zshrc"
-alias gi="cp ~/Templates/.gitignore .gitignore"
+alias vimrc="vim ~/.oh-my-zsh/custom/conorbot.zsh"
 
+pyinit() {
+	# first arg is directory, or "." by default. all other args ignored
+	dir="${1:-.}"
+	uv init "$dir"
+	cp -r ~/Templates/python-repo/. "$dir"
+}
 
 # uv is here, among maybe other things
 PATH="$PATH:$HOME/.local/bin"
 
 # ros stuff
-source /opt/ros/kilted/setup.zsh
-
 ## for autocomplete
 autoload -U +X compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 eval "$(register-python-argcomplete ros2)"
 
 
-## ROBOTS
+## ROBOTS/workspaces
+# top-level ros source. must be done before all else:
+alias source-ros="source /opt/ros/kilted/setup.zsh"
 # interbotix Pincher X100
 alias source-pincher="source ~/ws/interbotix/install/setup.zsh"
+
+alias source-hw1="source ~/Documents/elwin_ME495/hw1-ros-ws/install/setup.zsh"
